21501aa0602b8660a20822513c1bfdb7fd384f11
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-05 15:53:50 -0500

    bug fix - Full User Auth Backend

--- sahara/app/models/user.rb
+++ sahara/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
    before_validation :ensure_session_token
    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-   validates :password_digest, :session_token, :first_name, :last_name presence: true
+   validates :password_digest, :session_token, :first_name, :last_name, presence: true
    validates :password, length: { minimum: 6 }, allow_nil: true
 
    def self.find_by_credentials(email, password)
@@ -15,7 +15,7 @@ class User < ApplicationRecord
       nil
    end
 
-   def ensure_sesion_token
+   def ensure_session_token
       self.session_token ||= SecureRandom.urlsafe_base64
    end
 

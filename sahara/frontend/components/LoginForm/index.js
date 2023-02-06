2a8d32b1a98bb94b6f6870e06ad60a9790e5f3a4
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-05 23:45:18 -0500

    most of session front end creation, want to look through end of phase 1 and make sure the code makes sense before continuing forward.

--- sahara/frontend/components/LoginForm/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import * as sessionActions from '../../src/store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-
-function LoginForm() {
-   const dispatch = useDispatch();
-   
-}

0ddbb821e9ee549d63a3a4f2ebef33c55440036a
Charles Tyra <charlestyra@MacBook-Pro-3.local>
2023-02-07 15:47:19 -0500

    changed css styling for each SignUp and Login Forms

--- frontend/src/components/SignUpForm/index.js
+++ frontend/src/components/SignUpForm/index.js
@@ -5,12 +5,14 @@ import { Redirect } from 'react-router-dom';
 import './SignUpForm.css'
 import logo from '../../assets/images/amazon_logo.png'
 
-function SignUpForm() {
+function LoginForm() {
    const dispatch = useDispatch();
    const sessionUser = useSelector(state => state.session.user);
+
    const [name, setName] = useState('');
    const [email, setEmail] = useState('');
    const [password, setPassword] = useState('');
+   const [confirm, setConfirm] = useState('');
    const [errors, setErrors] = useState([]);
 
    if (sessionUser) return <Redirect to="/" />
@@ -37,58 +39,66 @@ function SignUpForm() {
    const puppies = 'https://www.shutterstock.com/search/puppy?c3apidt=71700000027388020&gclid=CjwKCAiAioifBhAXEiwApzCztoRJEEKIyoTaWE07f1LTlGkZHNU9D9Rc-fwZ73LhyrgC-q3m_aZDlBoCHgYQAvD_BwE&gclsrc=aw.ds&kw='
 
    return (
-      <>
+      <div id='signup-page'>
          <div id='nav-bar'>
             <img src={logo} id='nav-bar-logo' alt='' />
          </div>
-         <div id='login-page-container'>
-            <h2>Create Account</h2>
+         <div id='signup-page-container'>
+            <h2>Create account</h2>
             <form onSubmit={handleSubmit}>
                <ul>
                   {errors.map(error => <li key={error}>{error}</li>)}
                </ul>
-               <label className='amber-thick'>
-                  Your Name
-                  <br />
-                  <input
-                     type='text'
-                     value={name}
-                     onChange={e => setName(e.target.value)}
-                     className='amber-light'
-                  />
-               </label>
+               <label className='amber-thick'>Your name</label>
                <br />
-               <label className='amber-thick'>
-                  Email
-                  <br />
-                  <input
-                     type='text'
-                     value={email}
-                     onChange={e => setEmail(e.target.value)}
-                     className='amber-light'
-                  />
-               </label>
+               <input
+                  type='text'
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                  className='amber'
+                  id='name'
+               />
                <br />
-               <label>
-                  Password
-                  <br />
-                  <input
-                     type='password'
-                     value={password}
-                     onChange={e => setPassword(e.target.value)}
-                     required
-                  />
-               </label>
+               <label className='amber-thick'>Email</label>
                <br />
-               <button type='submit'>Continue</button>
+               <input
+                  type='text'
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  className='amber'
+               />
+               <br />
+               <label className='amber-thick'>Password</label>
+               <br />
+               <input
+                  type='password'
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  className='amber'
+                  required
+               />
+               <br />
+               <label className='amber-thick'>Re-enter password</label>
+               <br />
+               <input
+                  type='password'
+                  value={confirm}
+                  onChange={e => setConfirm(e.target.value)}
+                  className='amber'
+                  required
+               />
+               <br />
+               <button type='submit' className="amber">Continue</button>
             </form>
-            <div className="amber-thin" id="privacy-notice">
-               By creating an account, you agree to Charles' <a href={github}>Github informational center
-               </a> and <a href={puppies}>Puppy emporium</a>
+            <div className="amber" id="privacy-notice">
+               By creating an account, you agree to Charles' <a href={github}>Github Data Folder
+               </a> and <a href={puppies}>Puppy Emporium</a>.
             </div>
+            <div id='gradient-divider' />
+            <div class='amber'>Already have an account? <a href='/login'>Sign in</a></div>
          </div>
-      </>
+      </div>
    )
 }
 
-export default SignUpForm;
+export default LoginForm;

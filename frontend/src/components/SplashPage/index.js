31cb8e783c592df092a4845cd5a347450409a339
Charles Tyra <charlestyra@MacBook-Pro-3.local>
2023-02-09 09:49:51 -0500

    creation of NavBar component

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,23 +1,14 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import * as sessionActions from '../../store/session';
+import NavBar from "./NavBar";
 
 
 function SplashPage() {
    const dispatch = useDispatch();
 
-   const handleLogout = (e) => {
-      e.preventDefault();
-
-      dispatch(sessionActions.logout());
-   }
-
    return (
       <>
-         <a href="/login">Log In</a>
-         <br />
-         <a href="/signup">Sign Up</a>
-         <button onClick={handleLogout}>Log Out</button>
+         <NavBar />
       </>
    )
 }

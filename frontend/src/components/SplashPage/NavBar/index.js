7016db7ab9460259c9bd7a1f4b22fde44532aa2f
Charles Tyra <charlestyra@MacBook-Pro-3.local>
2023-02-14 10:16:04 -0500

    css fix for splash page

--- frontend/src/components/SplashPage/NavBar/index.js
+++ frontend/src/components/SplashPage/NavBar/index.js
@@ -10,28 +10,32 @@ function NavBar() {
    return (
       <>
          <div id='nav-bar'>
-            <div id='nav-bar-logo-container'>
-               <a href='/'>
-                  <img src={logo} id='nav-bar-logo' alt='' />
-               </a>
-            </div>
-            <div id='welcome-address-container'>
-               <h2 className='nav-bar-h2'>Welcome to sahara,</h2>
-               <div className="nav-bar-text">Make yourself at home</div>
+            <div id='left-nav-container'>
+               <div id='nav-bar-logo-container'>
+                  <a href='/'>
+                     <img src={logo} id='nav-bar-logo' alt='' />
+                  </a>
+               </div>
+               <div id='welcome-address-container'>
+                  <h2 className='nav-bar-h2'>Welcome to sahara,</h2>
+                  <div className="nav-bar-text">Make yourself at home</div>
+               </div>
             </div>
             <div id='nav-search-container'>
                <NavSearchBar />
             </div>
-            <div id='session-button-container'>
-               <SessionActionButton />
-            </div>
-            <div id='order-list-container'>
-               <h2 className="nav-bar-h2"> Returns </h2>
-               <div className='nav-bar-text'>& Orders</div>
-            </div>
-            <div id='cart-container'>
-               <div id='cart-number'>0</div>
-               <img src={cart} id='nav-bar-cart' alt="" />      
+            <div id='right-nav-container'>
+               <div id='session-button-container'>
+                  <SessionActionButton />
+               </div>
+               <div id='order-list-container'>
+                  <h2 className="nav-bar-h2"> Returns </h2>
+                  <div className='nav-bar-text'>& Orders</div>
+               </div>
+               <div id='cart-container'>
+                  <div id='cart-number'>0</div>
+                  <img src={cart} id='nav-bar-cart' alt="" />      
+               </div>
             </div>
          </div>
       </>

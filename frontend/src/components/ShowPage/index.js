812eeaf3beb87d39818e2c392bec2e670885dd3d
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-17 00:20:18 -0500

    Dimensions for the show page, redid a bit of the css, going to fill out the cart area a bit with placeholder things

--- frontend/src/components/ShowPage/index.js
+++ frontend/src/components/ShowPage/index.js
@@ -8,7 +8,13 @@ import Footer from '../Footer';
 import bells from '../../assets/images/bells.png';
 import ratings from '../../assets/images/rating_placeholder.png'
 
-import twoxtwo from '../../assets/'
+import twoxone from '../../assets/images/2x1.png'
+import twoxtwo from '../../assets/images/2x2.png'
+import threextwo from '../../assets/images/3x2.png'
+import threexthree from '../../assets/images/3x3.png'
+import fourxthree from '../../assets/images/4x3.png'
+import fourxfour from '../../assets/images/4x4.png'
+import fivexfive from '../../assets/images/5x5.png'
 
 function ShowPage() {
    const { itemId } = useParams();
@@ -24,6 +30,15 @@ function ShowPage() {
          null
       )
    } else {
+      let dimensions = null;
+      if (item.dimensions === 'twoxtwo') dimensions = twoxtwo;
+      else if (item.dimensions === 'twoxone') dimensions = twoxone;
+      else if (item.dimensions === 'threextwo') dimensions = threextwo;
+      else if (item.dimensions === 'threexthree') dimensions = threexthree;
+      else if (item.dimensions === 'fourxthree') dimensions = fourxthree;
+      else if (item.dimensions === 'fourxfour') dimensions = fourxfour;
+      else if (item.dimensions === 'fivexfive') dimensions = fivexfive;
+
    return (
       <> 
          <div id='show-page-container'> 
@@ -32,13 +47,18 @@ function ShowPage() {
             </div>
             <div id='show-info-container'>
                <h2>{item.itemName}</h2>
-               <img className='ratings' src={ratings} />
-               <div></div>
+               <img className='ratings' src={ratings} /> 40 ratings
                <hr/>
-               <div id='description-div'>{item.description}</div>
                <div><span className='description-span'>Colors</span>{item.colors}</div>
                <div><span className='description-span'>Room Type</span>{item.theme}</div>
                <div><span className='description-span'>Material</span>{item.material}</div>
+               <div><span className='description-span'>Dimensions</span><img src={dimensions}/></div>
+               <hr/>
+               <div id='description-div'>
+                  <h4>About this item</h4><br/>
+                  {item.description}
+               </div>
+
             </div>
             <div id='show-cart-container'>
                <div id='price-info'>

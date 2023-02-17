a42bf8f661e86537f5d3180c7936b7989dfea336
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-17 00:30:14 -0500

    finished footer, wrote css in showpage - realized and put it into Footer.css, will show in next update

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -8,18 +8,21 @@ function Footer() {
          <div id='to-the-top'>Back to top</div>
          <div id='footer-container'>
             <div className="footer">
+               <h2>Previous towns</h2>
+               Hersheyvill <br/> Caliban <br/> Elizaville
+            </div>
+            <div className="footer">
                <h2>Languages</h2>
-               React <br/> Javascript <br/> Ruby <br/> Rails <br/> CSS
+               React <br/> Javascript <br/> Ruby <br/> CSS
             </div>
             <div className="footer">
-               <h2></h2>
+               <h2>Gemfiles</h2>
+               byebug <br/>bcrypt <br/>ruby '3.1.1' <br/>rails '7.0.4' <br/>aws-sdk-s3
             </div>
             <div className="footer">
-               <h2>Previous town names</h2>
-               Hersheyvill <br/> Caliban
-            </div>
-            <div className="footer">
-
+               <h2>Contact me</h2>
+               <a href='https://github.com/charles-tyra'>Github</a> <br/>
+               <a href='https://github.com/charles-tyra'>Linked-in</a>
             </div>
          </div>
          <div id='footer-divider'/>

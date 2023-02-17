c489161b803ee16305bcf08e19dd7c6b1744d3a6
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2023-02-16 23:02:34 -0500

    base footer created, going to link github in darker footer

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -71,6 +71,24 @@ function SplashPage() {
                )
             })}
          </div>
+         <div id='to-the-top'>Back to top</div>
+         <div id='footer-container'>
+            <div className="footer">
+               <h2>Languages</h2>
+               React <br/> Javascript <br/> Ruby <br/> Rails <br/> CSS
+            </div>
+            <div className="footer">
+               <h2></h2>
+            </div>
+            <div className="footer">
+               <h2>Previous town names</h2>
+               Hershey <br/> Caliban
+            </div>
+            <div className="footer">
+
+            </div>
+         </div>
+         <div id='footer-divider'/>
       </>
    )} 
    }

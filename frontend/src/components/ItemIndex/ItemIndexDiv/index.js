ee661db874b6b38eb1c5491c426bbebcb21524e1
Charles Tyra <charles.tyra@gmail.com>
2023-03-29 19:40:38 -0400

    Nav Bar Link to /items

--- frontend/src/components/ItemIndex/ItemIndexDiv/index.js
+++ frontend/src/components/ItemIndex/ItemIndexDiv/index.js
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './ItemIndexDiv.css';
 import Footer from "../../Footer";
+import { Link } from "react-router-dom";
 
 function ItemIndexDiv( {item} ) {
    console.log(item);
 
    return (
       <div className="item-index-div">
-         <a className="item-index-image-container" href={`/items/${item.id}`}>
-            <img className="item-index-image" src={item.photoUrls[1]}/>
-         </a>
+         <div className="item-index-image-container">
+            <Link to={`/items/${item.id}`}> 
+               <img className="item-index-image" src={item.photoUrls[1]} />
+            </Link>
+         </div>
          <a className='item-index-name' href={`/items/${item.id}`}>{item.itemName}</a>
       </div>
    )
--- frontend/src/components/SplashPage/NavBar/NavSearchBar/index.js
+++ frontend/src/components/SplashPage/NavBar/NavSearchBar/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './NavSearchBar.css';
 
 function NavSearchBar() {
 
    return (
       <>
-         <form>
+         <form id='search-form'>
             <input id='nav-search-input'></input>
+               <Link to={`/items`}>
+            <button type="submit" id='nav-bar-submit'/> 
+
+               </Link>
          </form>
       </>
    )

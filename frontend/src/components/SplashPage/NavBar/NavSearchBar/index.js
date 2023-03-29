8e3b029a5f8e8005701e6e17a9d165394ee46198
Charles Tyra <charles.tyra@gmail.com>
2023-03-29 19:53:09 -0400

    Search Icon for button

--- frontend/src/components/SplashPage/NavBar/NavSearchBar/index.js
+++ frontend/src/components/SplashPage/NavBar/NavSearchBar/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './NavSearchBar.css';
+import Search from '../../../../assets/images/search_icon.png'
 
 function NavSearchBar() {
 
    return (
       <>
          <form id='search-form'>
-            <input id='nav-search-input'></input>
+            <input id='nav-search-input' />
                <Link to={`/items`}>
-            <button type="submit" id='nav-bar-submit'/> 
-
+                  <button type="submit" id='nav-bar-submit'>
+                     <img id='search-icon' src={Search}/>   
+                  </button> 
                </Link>
          </form>
       </>

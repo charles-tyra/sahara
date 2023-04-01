d75c42f8775091695ccb2ee4f2b30f4cb1b1b47a
Charles Tyra <charles.tyra@gmail.com>
2023-03-31 20:06:04 -0400

    More Item Index CSS

--- frontend/src/components/ItemIndex/index.js
+++ frontend/src/components/ItemIndex/index.js
@@ -9,6 +9,7 @@ import ItemIndexDiv from "./ItemIndexDiv";
 function ItemIndex() {
    const dispatch = useDispatch();
    const items = useSelector(getItems);
+   const [sortPriority,setSortPriority] = useState('test')
 
    useEffect(() => {
       dispatch(fetchItems());
@@ -24,13 +25,23 @@ function ItemIndex() {
       return (
          <>
             <div id='item-index-header'>
-               Results
+               <div id='header-container'>
+                  <div id="results-container">
+                     {items.length} results for&nbsp;<span id='search-placeholder'>"SEARCH PARAMS PLACEHOLDER"</span>
+                  </div>
+                  <select id='search-select' value={``}> 
+                     <option value=''> Sort by: {sortPriority}</option>
+                  </select>
+               </div>
             </div>
             <div id='item-index-page'>
                <div id='filter-sidebar'>
-                  Testing
+                  testing
                </div>
                <div id='item-index-item-container'>
+                  <div id='item-index-item-header'>
+                     
+                  </div>
                   {items.map((item, i) => (<ItemIndexDiv item={item} key={item.id} />))}
                </div>
             </div>

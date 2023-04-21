e4360c3b876c660a4db50f720c956ebf3d72d23d
Charles Tyra <charles.tyra@gmail.com>
2023-04-21 16:27:07 -0400

    Base Cart Index Form Css

--- frontend/src/components/CartIndex/CartIndexForm/index.js
+++ frontend/src/components/CartIndex/CartIndexForm/index.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateCart, deleteCart } from "../../../store/carts";
 import { getItem, fetchItem } from "../../../store/items";
-import './CartIndexForm.css'
 import { Link } from "react-router-dom";
+
+import './CartIndexForm.css'
+import bells from '../../../assets/images/bells.png';
 
 
 const CartIndexForm = ({ cartItem }) => {
@@ -34,47 +36,55 @@ const CartIndexForm = ({ cartItem }) => {
                <Link to={`/items/${item.id}`}>
                   {item.itemName}
                </Link>
-               <form className='cart-form'>
-                  test
-               </form>
-               <div className='cart-delete' onClick={() => dispatch(deleteCart(cartItem.id))}>
-                  Delete Me
+               <div className="green-stock">
+                  In Stock
                </div>
-               <select value={cartItem.quantity} onChange={handleUpdate}>
-                  <option value='1'>1</option>
-                  <option value='2'>2</option>
-                  <option value='3'>3</option>
-                  <option value='4'>4</option>
-                  <option value='5'>5</option>
-                  <option value='6'>6</option>
-                  <option value='7'>7</option>
-                  <option value='8'>8</option>
-                  <option value='9'>9</option>
-                  <option value='10'>10</option>
-                  <option value='11'>11</option>
-                  <option value='12'>12</option>
-                  <option value='13'>13</option>
-                  <option value='14'>14</option>
-                  <option value='15'>15</option>
-                  <option value='16'>16</option>
-                  <option value='17'>17</option>
-                  <option value='18'>18</option>
-                  <option value='19'>19</option>
-                  <option value='20'>20</option>
-                  <option value='21'>21</option>
-                  <option value='22'>22</option>
-                  <option value='23'>23</option>
-                  <option value='24'>24</option>
-                  <option value='25'>25</option>
-                  <option value='26'>26</option>
-                  <option value='27'>27</option>
-                  <option value='28'>28</option>
-                  <option value='29'>29</option>
-                  <option value='30'>30</option>
-               </select>
-            </div>
+               <div className="cart-info">
+                  <span>Size:</span> {item.dimensions}
+               </div>
+               <div className="cart-info">
+                  <span>Color:</span> {item.colors}
+               </div>
+               <div>
+                  <select value={cartItem.quantity} onChange={handleUpdate}>
+                     <option value='1'>1</option>
+                     <option value='2'>2</option>
+                     <option value='3'>3</option>
+                     <option value='4'>4</option>
+                     <option value='5'>5</option>
+                     <option value='6'>6</option>
+                     <option value='7'>7</option>
+                     <option value='8'>8</option>
+                     <option value='9'>9</option>
+                     <option value='10'>10</option>
+                     <option value='11'>11</option>
+                     <option value='12'>12</option>
+                     <option value='13'>13</option>
+                     <option value='14'>14</option>
+                     <option value='15'>15</option>
+                     <option value='16'>16</option>
+                     <option value='17'>17</option>
+                     <option value='18'>18</option>
+                     <option value='19'>19</option>
+                     <option value='20'>20</option>
+                     <option value='21'>21</option>
+                     <option value='22'>22</option>
+                     <option value='23'>23</option>
+                     <option value='24'>24</option>
+                     <option value='25'>25</option>
+                     <option value='26'>26</option>
+                     <option value='27'>27</option>
+                     <option value='28'>28</option>
+                     <option value='29'>29</option>
+                     <option value='30'>30</option>
+                  </select>
+                  <div className='cart-delete' onClick={() => dispatch(deleteCart(cartItem.id))}>
+                     Delete Me
+                  </div>
+               </div>
+            `</div>
             <div className="cart-item-price">
-               {item.bells}
+               <img className='cart-bells' src={bells}/>{item.bells}
             </div>
          </div>
       </>

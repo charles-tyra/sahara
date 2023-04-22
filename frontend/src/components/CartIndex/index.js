0ddee7fff1a39f3a96fb7c4bb16b1454c89976b1
Charles Tyra <charles.tyra@gmail.com>
2023-04-22 12:09:12 -0400

    Empty Cart Index

--- frontend/src/components/CartIndex/index.js
+++ frontend/src/components/CartIndex/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCarts, fetchCarts } from '../../store/carts';
 import money from '../../assets/images/money-clip-art-38.jpg';
+import bells from '../../assets/images/bells.png'
 import './CartIndex.css';
 import { Link } from 'react-router-dom';
 import CartIndexForm from './CartIndexForm';
@@ -24,7 +25,7 @@ function CartIndex() {
                   <img id='cart-image' src={money} />
                </div>
                <div id='cart-session-info'>
-                  <h2>Your Sahara Cart is empty</h2>
+                  <h2>Your Sahara Cart is empty.</h2>
                   <Link id='cart-deals' to={'/items'}>Shop today's deals</Link>
                   <div id='cart-session-buttons-container'>
                      <Link to={'/login'} id='cart-login-container'>
@@ -38,18 +39,41 @@ function CartIndex() {
             </div>
          </div>
       )
+   } else if (carts.length === 0) {
+      return (
+         <div id='cart-background'>
+            <div id='cart-session' className='empty-cart'>
+               <div id='cart-session-info'>
+                  <h2>Your Sahara Cart is empty.</h2>
+                  <Link id='cart-deals' to={'/items'}>Shop today's deals</Link>
+               </div>
+            </div>
+         </div>
+      )
    } else {
-      let subtotal = 0;
-      for (let i = 0; i < carts.length; i++) subtotal = subtotal + (parseInt(carts[i].quantity) *parseInt(carts[i].item.bells));
-
-      console.log(subtotal);
       const showSubtotal = boolean => {
+         let subtotal = 0;
+         let itemTotal = 0;
+         for (let i = 0; i < carts.length; i++) {
+            subtotal = subtotal + (parseInt(carts[i].quantity) *parseInt(carts[i].item.bells));
+            itemTotal = itemTotal + parseInt(carts[i].quantity);
+         }
+         
          if (boolean) {
             return (
-               <></>
+               <>
+                  <hr className='cart-form-spacer'/>
+                  <div className='subtotal-container'>
+                     Subtotal ({itemTotal} items): <span><img className='cart-bells' src={bells}/>{subtotal}</span>
+                  </div>
+               </>
             )
          } else {
-
+            return (
+               <div className='subtotal-container left-subtotal'>
+                  Subtotal ({itemTotal} items): <span><img className='cart-bells' src={bells} />{subtotal}</span>
+               </div>
+            )
          }
       }
 
@@ -59,9 +83,10 @@ function CartIndex() {
             <div id='cart-index-container'> 
                <h2>Shopping Cart</h2>
                {carts !== [] ? carts.map(cartItem => <CartIndexForm cartItem={cartItem} key={cartItem.id} />) : null}
+               {carts.length !== 0 ? showSubtotal(true) : null}
             </div>
             <div id='cart-checkout'> 
-               Subtotal
+               {carts.length !== 0 ? showSubtotal(false) : null }
             </div>
          </div>
       )

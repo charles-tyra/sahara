aca9afbae1bdb02baced561a72dd51ba5c51bee2
Charles Tyra <charles.tyra@gmail.com>
2023-04-28 22:23:17 -0400

    Base Cart Checkout CSS

--- frontend/src/components/CartCheckout/index.js
+++ frontend/src/components/CartCheckout/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, useLocation } from "react-router-dom";
 
+import './CartCheckout.css'
+import checkmark from '../../assets/images/green_checkmark.png';
+
 const CartCheckout = () => {
    const location = useLocation();
    const carts = location.state?.carts;
@@ -9,9 +12,25 @@ const CartCheckout = () => {
 
    if (!currentUser || !carts) return <Redirect to='/carts' />
    
+   console.log(currentUser)
    return (
-      <div id='cart-background'>
+      <div id='checkout-background'>
+         <div id='order-placed-container'>
+            <div id='order-placed'>
+               <div> 
+                  <img src={checkmark} id='green-checkmark' /> Order placed, thanks!
+               </div>
+               Confirmation will be sent by pelican or camel, depending on location.
+            </div>
+            <div id='checkout-index-container'>
+               <div id='shipping-div'>
 
+               </div>
+            </div>
+         </div>
+         <div id='checkout-ad'>
+            test
+         </div>
       </div>
    )
 }

af9032a4d5b64b4bcee0415ca5f952f212ec77ab
Charles Tyra <charles.tyra@gmail.com>
2023-05-03 01:28:10 -0400

    Base Create Review Page skele - now to work on specific CSS

--- frontend/src/components/CreateReviewPage/index.js
+++ frontend/src/components/CreateReviewPage/index.js
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createReview } from "../../store/reviews";
+import { Redirect, useParams } from "react-router-dom";
 import ReactStars from 'react-stars';
 import './CreateReviewPage.css';
-import { Redirect, useParams } from "react-router-dom";
+import { fetchItem, getItem } from "../../store/items";
 
 const CreateReviewPage = () => {
    const dispatch = useDispatch();
-   const currentUser = useSelector(state => state.session.user);
    const {itemId} = useParams();
+   const currentUser = useSelector(state => state.session.user);
+   const item = useSelector(getItem(itemId));
 
    const [title, setTitle] = useState('');
    const [body, setBody] = useState('');
    const [rating, setRating] = useState(0);
 
-   if(!currentUser || !itemId) return <Redirect to='/' />
+   useEffect(() => {
+      dispatch(fetchItem(itemId));
+   }, [])
+   
+   if(!currentUser) return <Redirect to='/' />
 
    const handleSubmit = () => {
       return dispatch(createReview({
@@ -25,16 +31,33 @@ const CreateReviewPage = () => {
          rating
       }))
    }
+   console.log(item);
+
+   if(!item) return null;
 
    return (
-   <form onSubmit={handleSubmit}>
-      <ReactStars count={5} size={24} half={false} value={rating} onChange={e => setRating(e)} />
-      title
-      <input onChange={e => setTitle(e.target.value)} />
-      body
-      <input onChange={e => setBody(e.target.value)} />
-      <button type="submit">submit button</button>
-   </form>
+      <div id='create-review-page-container'>
+         <h4 id='create-review-header'>Create Review</h4>
+         <div id='create-review-item-info'>
+            <img id='review-image'src={item.photoUrls[0]}/>
+            <span>{item.itemName}</span>
+         </div>
+         <hr className="create-review-space image-space"/>
+
+         <h5>Overall Rating</h5>
+         <ReactStars count={5} size={24} half={false} value={rating} onChange={e => setRating(e)} />
+         <hr className="create-review-space"/>
+
+         <h5>Add a headline</h5>
+         <input placeholder="What's most important to know?" onChange={e => setTitle(e.target.value)} />
+         <hr className="create-review-space"/>
+
+         <h5>Add a written review</h5>
+         <input onChange={e => setBody(e.target.value)} />
+         <hr className="create-review-space"/>
+
+         <button type="submit" onClick={handleSubmit}>Submit</button>
+      </div>
    )
 }
 

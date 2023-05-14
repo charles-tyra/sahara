244dd368c3b3310d03534b8477c2e8fad676d33e
Charles Tyra <charles.tyra@gmail.com>
2023-05-13 20:30:35 -0400

    Review AVG on Item Index

--- frontend/src/components/ItemIndex/ItemIndexDiv/index.js
+++ frontend/src/components/ItemIndex/ItemIndexDiv/index.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getReviews, fetchReviews } from '../../../store/reviews';
-
+import React from "react";
 import './ItemIndexDiv.css';
 import { Link } from "react-router-dom";
 
 import bells from '../../../assets/images/bells.png';
 import ratings from '../../../assets/images/rating_placeholder.png'
+import ReactStars from "react-stars";
 
 function ItemIndexDiv( {item} ) {
+   let reviewSum = 0;
+   item.reviews.map(review => reviewSum += review.rating)
+
+   item.reviews.length ? reviewSum = reviewSum / item.reviews.length : reviewSum = 0;
 
    return (
       <div className="item-index-div">
@@ -20,7 +22,11 @@ function ItemIndexDiv( {item} ) {
          <div className="item-index-div-info">
             <a className='item-index-name' href={`/items/${item.id}`}>{item.itemName}</a>
             <div className='item-index-color'>{item.colors}</div>
-            5.0 <img className='ratings' src={ratings} alt='' /> (49) 
+            <div className="item-index-reviews">
+               <div className="review-blocker index-blocker"/>
+               <ReactStars className='index-star' value={reviewSum} /> <span>({item.reviews.length}) </span>
+               <ReactStars className="index-star-backer" value={5}/>
+            </div>
             <div id='price-info'>
                <h3><img className='bells' src={bells} alt='' /> {item.bells}</h3>
             </div>
